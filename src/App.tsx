4ad1382c8eb6bf309f1f00fd911a063f342d069a
VanshChauhan15 <97740365+VanshChauhan15@users.noreply.github.com>
2024-04-24 23:59:02 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {SignedIn, SignedOut, SignInButton, UserButton, UserProfile} from '@clerk/clerk-react'
+import RootLayout from './layouts/RootLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
